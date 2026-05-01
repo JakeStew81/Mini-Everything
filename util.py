942d9e33008b7845e5d3a155752614b68b94c6c9
JakeStew81 <jakestewart711@icloud.com>
2026-05-01 11:02:14 -0400

    Adjusted values & added name vs displayname

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 class NodeType:
-    def __init__(self, name: str, needs: dict[str, tuple[int, int]], maxPeople: int):
+    def __init__(self, name: str, displayName: str, needs: dict[str, tuple[int, int]], maxPeople: int):
         # For tuples, format is (people, goods)
         self.name = name
+        self.displayName = displayName
         self.needs = needs
         self.maxPeople = maxPeople
 
@@ -11,22 +12,22 @@ class ConnectionType:
         self.capacity = capacity
 
 nodeTypes = {
-    "center": NodeType("City Center",
+    "center": NodeType("center", "City Center",
                        {"c": (0, 0), "r": (0, 0), "m": (0, 0), "i": (0, 0), "o": (0, 0)},
                        9999),
-    "residential": NodeType("Residential",
-                            {"c": (1, 0), "r": (0, 0), "m": (2, 0), "i": (2, 0), "o": (1, 0)},
-                            -1),
-    "market": NodeType("Commercial",
-                       {"c": (0, 0), "r": (0, 0), "m": (0, 0), "i": (0, 2), "o": (0, 2)},
+    "residential": NodeType("residential", "Residential",
+                            {"c": (1, 0), "r": (0, 0), "m": (2, 0), "i": (0, 0), "o": (1, 0)},
+                            2),
+    "market": NodeType("market", "Commercial",
+                       {"c": (0, 0), "r": (0, 0), "m": (0, 0), "i": (0, 1), "o": (0, 1)},
                        10),
-    "industry": NodeType("Industrial",
-                         {"c": (0, 0), "r": (0, 0), "m": (0, 0), "i": (0, 0), "o": (0, 2)},
-                         10),
-    "junction": NodeType("Junction",
+    "industry": NodeType("industrial", "Industrial",
+                         {"c": (1, 0), "r": (2, 0), "m": (0, 0), "i": (2, 0), "o": (0, 2)},
+                         0),
+    "junction": NodeType("junction", "Junction",
                          {"c": (0, 0), "r": (0, 0), "m": (0, 0), "i": (0, 0), "o": (0, 0)},
                          9999),
-    "out": NodeType("Outside of City",
+    "out": NodeType("out", "Outside of City",
                     {"c": (0, 0), "r": (0, 0), "m": (0, 0), "i": (0, 0), "o": (0, 0)},
                     9999),
 }  # TODO: Fill out properly with good values & stuff. Temp values for testing rn.
